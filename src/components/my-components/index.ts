6f444327b032c10ddda25804f27134b84b0e828e
Juan José Lopera Londoño <Developera0612@gmail.com>
2024-11-03 19:13:58 -0500

    Mejoras en el código y la UI

---
+++ src/components/my-components/index.ts
@@ -0,0 +1,5 @@
+// Definimos un archivo index.ts que exporta todos los componentes de la carpeta my-components (archivo de barril)
+
+export * from './Header';
+export * from './Inmuebles';
+export * from './TipoInmueble';
